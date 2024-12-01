9c0dd2041d24262035a20a1c0037258ecc978b0a
minkyung01 <84274527+minkyung01@users.noreply.github.com>
2024-12-01 23:53:31 +0900

    signin 시, req validation 제거

--- src/module/dto/signin.dto.ts
+++ src/module/dto/signin.dto.ts
@@ -1,42 +1,14 @@
-import { ArrayNotEmpty, IsNotEmpty, IsString } from 'class-validator';
 import { Students } from '../entity/students.entity';
 
 export class SigninRequest {
-  @IsNotEmpty()
-  @IsString()
   readonly name: string;
-
-  @IsNotEmpty()
-  @IsString()
   readonly nationality: string;
-
-  @IsNotEmpty()
-  @IsString()
   readonly email: string;
-
-  @IsNotEmpty()
-  @IsString()
   readonly password: string;
-
-  @IsNotEmpty()
-  @IsString()
   readonly education: string;
-
-  @IsNotEmpty()
-  @IsString()
   readonly major: string;
-
-  @IsNotEmpty()
-  @IsString()
   readonly visa: string;
-
-  @IsNotEmpty()
-  @IsString()
   readonly topikLevel: string;
-
-  @IsNotEmpty()
-  @ArrayNotEmpty()
-  @IsString({ each: true })
   readonly tags: string[];
 }
 
