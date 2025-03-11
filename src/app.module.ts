e724d6b0550f2272036f0e2309ce537008d0abb8
minkyung01 <84274527+minkyung01@users.noreply.github.com>
2025-03-11 11:53:04 +0900

    set database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,35 +4,21 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Job } from './module/entity/job.entity';
 import { Student } from './module/entity/student.entity';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-import * as process from 'process';
 import { Company } from './module/entity/company.entity';
 import { Letter } from './module/entity/letter.entity';
 import { Portfolio } from './module/entity/portfolio.entity';
 import { Resume } from './module/entity/resume.entity';
 import { Score } from './module/entity/score.entity';
 import { StudentJob } from './module/entity/student_job.entity';
+import { databaseConfig } from './common/database.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_USERNAME,
-      database: 'triplej',
-      entities: [
-        Company,
-        Job,
-        Letter,
-        Portfolio,
-        Resume,
-        Score,
-        Student,
-        StudentJob,
-      ],
-      synchronize: false,
-      namingStrategy: new SnakeNamingStrategy(),
+    ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }),
+    TypeOrmModule.forRootAsync({
+      imports: undefined,
+      useFactory: databaseConfig,
     }),
     TypeOrmModule.forFeature([
       Company,
